3bc0c04ac5eb21943457a58ff8af4282b3c6060c
Stas-Dolgov <145658240+Stas-Dolgov@users.noreply.github.com>
2025-04-18 15:39:06 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import random
+import secrets
 import re
-from flask import Flask, render_template, request, make_response, redirect, url_for
+from flask import Flask, render_template, request, make_response, redirect,  url_for, session, flash
 from faker import Faker
 from werkzeug.exceptions import BadRequest
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_required, login_user, current_user
+
 
 fake = Faker()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] =  secrets.token_hex(16)
 application = app
 
 images_ids = ['7d4e9175-95ea-4c5f-8be5-92a6b708bb3c',
@@ -14,6 +19,28 @@ images_ids = ['7d4e9175-95ea-4c5f-8be5-92a6b708bb3c',
               '6e12f3de-d5fd-4ebb-855b-8cbc485278b7',
               'afc2cfe7-5cac-4b80-9b9a-d5c65ef0c728',
               'cab5b7f2-774e-4884-a200-0c0180fa777f']
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+login_manager.login_message = "Для доступа к этой странице необходимо пройти аутентификацию."
+
+class User(UserMixin):
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password  
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+users = {
+    1: User(1, "user", generate_password_hash("qwerty"))
+}
+
+@login_manager.user_loader
+def load_user(user_id):
+    return users.get(int(user_id))
 
 def generate_comments(replies=True):
     comments = []
@@ -35,6 +62,25 @@ def generate_post(i):
     }
 
 posts_list = sorted([generate_post(i) for i in range(5)], key=lambda p: p['date'], reverse=True)
+
+def format_phone_number(phone_number):
+    digits = re.sub(r'\D', '', phone_number)
+    country_code = ""
+    
+    if len(digits) == 10:
+        country_code = "+7"
+    elif digits.startswith('7'):
+        country_code = "+7"
+        digits = digits[1:]
+    elif digits.startswith('8'):
+        country_code = "+7"
+        digits = digits[1:]
+
+    if len(digits) == 10 or (len(digits) == 11 and digits.startswith(('2', '3', '4', '5', '6', '7', '8', '9'))):
+        return f"{country_code} ({digits[:3]}) {digits[3:6]}-{digits[6:8]}-{digits[8:10]}"
+
+    else:
+        return "Invalid phone number format"
 
 def format_phone_number(phone_number):
     digits = re.sub(r'\D', '', phone_number)
@@ -125,6 +171,38 @@ def phone():
         return render_template('phone.html', formatted_number=formatted_number, phone_number=phone_number)
 
     return render_template('phone.html')
+
+@app.route('/visits')
+def visits():
+    if 'visits' in session:
+        session['visits'] = session.get('visits') + 1
+    else:
+        session['visits'] = 1
+    return render_template('visits.html', visits=session['visits'])
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        remember = True if request.form.get('remember') else False
+
+        user = next((u for u_id, u in users.items() if u.username == username), None)
+
+        if user and user.check_password(password):
+            login_user(user, remember=remember)
+            flash("Вы успешно вошли!", "success")
+            next_page = request.args.get('next')  
+            return redirect(next_page or url_for('index'))
+        else:
+            flash("Неверный логин или пароль.", "danger")
+
+    return render_template('login.html')
+
+@app.route('/secret')
+@login_required
+def secret():
+    return render_template('secret.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
